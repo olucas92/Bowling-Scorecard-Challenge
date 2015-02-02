c087817d32d23ef2da5312578c1a893a7296b60f
Oliver <o.lucas@live.com>
2015-02-02 13:00:07 +0000

    added a . that was missing to make tests work

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -12,7 +12,7 @@ describe ("Bowling", function(){
   describe ("by default", function(){
 
     it("starts off at a score of 0", function(){
-      expect(bowling.accumulativeScore)toEqual(0);
+      expect(bowling.accumulativeScore).toEqual(0)
     });
 
     it("knows there are 10 pins to knock down", function(){
